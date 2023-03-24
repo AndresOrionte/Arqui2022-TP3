ebc3904cd31469d7e1a38da84941c31ba3fa5a37
AndresOrionte <andresorionte@gmail.com>
2023-03-23 23:04:33 -0300

    first commit

---
+++ Arqui2022-TP3.srcs/sources_1/new/PC.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.03.2023 22:59:47
+// Design Name: 
+// Module Name: PC
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module PC(
+
+    );
+endmodule
