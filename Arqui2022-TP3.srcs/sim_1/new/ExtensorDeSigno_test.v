329ba058669e50fca4b40dc6b95e895ce19a942b
AndresOrionte <andresorionte@gmail.com>
2023-05-04 01:49:36 -0300

    Update: ExtensorDeSigno parametrized and tested

--- Arqui2022-TP3.srcs/sim_1/new/ExtensorDeSigno_test.v
+++ Arqui2022-TP3.srcs/sim_1/new/ExtensorDeSigno_test.v
@@ -1,21 +1,24 @@
 `timescale 1ns / 1ps
 
-module ExtensorDeSigno_test();
+module ExtensorDeSigno_test #(
+    parameter BUS_WIDTH = 28
+)();
 
-    reg [15:0] i_dato;
+    reg [BUS_WIDTH-1:0] i_dato;
     wire [31:0] o_dato;
 
-    ExtensorDeSigno extensor(i_dato, o_dato);
+    ExtensorDeSigno #(.BUS_WIDTH(BUS_WIDTH)) extensor(i_dato, o_dato);
     
     initial begin
-    
-    i_dato = 16'h8000;
-    #5
-    
-    i_dato = 16'h00FF;
-    #5
-    
-    i_dato = 16'hFFFF;
+
+        i_dato = 28'hFFFF00F;
+        #5;
+
+        i_dato = 28'h0000FFF;  
+        #5;
+
+        i_dato = 28'hFFFFFFF;
+        #5;
     
     end
 endmodule
--- Arqui2022-TP3.srcs/sources_1/new/ExtensorDeSigno.v
+++ Arqui2022-TP3.srcs/sources_1/new/ExtensorDeSigno.v
@@ -1,10 +1,12 @@
 `timescale 1ns / 1ps
 
-module ExtensorDeSigno(
-    input wire [15:0] i_dato,
+module ExtensorDeSigno #(
+    parameter BUS_WIDTH = 16
+)(
+    input wire [BUS_WIDTH-1:0] i_dato,
     output wire [31:0] o_dato
     );
     
-    assign o_dato = (i_dato[15] == 1) ? {16'b1111111111111111, i_dato} : {16'b0000000000000000, i_dato};
+    assign o_dato = (i_dato[BUS_WIDTH-1] == 1) ? {{BUS_WIDTH{1'b1}}, i_dato} : {{BUS_WIDTH{1'b0}}, i_dato};
     
 endmodule
